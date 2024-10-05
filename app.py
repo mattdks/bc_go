9a444862129e03f1d40eac3c500a51d377e8a023
Kit Levy <levyec@bc.edu>
2024-10-05 16:24:31 -0400

    Stored data in dict by category as dict by points

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask, render_template, request
 import pandas as pd
 
 def sort_data():
-    try:
-        df = pd.read_csv('testdata1.csv')
-        points = df["Points"]
-        return str(points)
-    except Exception as e:
-        return f"Error reading CSV: {str(e)}"
+    df = pd.read_csv('testdata1.csv')
+    categories = df["Category"]
+    points = df["Points"]
+    mission = df["Mission"]
+    bigdict={}
+    for i in range(1,4):
+        cat=categories[i*int(len(categories)/3)]
+        bigdict[cat]={1:[],2:[],3:[],4:[]}
+    for i in range(len(categories)):
+        print()
+        bigdict[categories[i]][int(points[i])].append(mission[i])
+
+    return str(bigdict)
 
 app = Flask(__name__)
 
