54c788fa19194e8643467e9a04797fc082bf5a68
Kit Levy <levyec@bc.edu>
2024-10-05 19:51:27 -0400

    Changed text to randomized arrangement of missions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import random
 
 def sort_data():
     df = pd.read_csv('testdata1.csv')
@@ -16,6 +17,18 @@ def sort_data():
 
     return bigdict
 
+def nine_events(dict):
+    events=[]
+    for key in dict:
+        for i in range(3):
+            pt=random.randint(1,4)
+            events.append((dict[key][pt][random.randint(0,len(dict[key][pt])-1)],pt))
+    random.shuffle(events)
+    return events
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,12 +37,37 @@ def home():
 
 @app.route('/run_code', methods=['POST'])
 def run_code():
-    dict=sort_data()
     result1 = None
+    event0 = None
+    event1 = None
+    event2 = None
+    event3 = None
+    event4 = None
+    event5 = None
+    event6 = None
+    event7 = None
+    event8 = None
+
+    dict=sort_data()
+    e=nine_events(dict)
+    events=[]
+    for i in e:
+        s=i[0]+" - "+str(i[1])+" points"
+        events.append(s)
+
     if request.method == 'POST':
         # Example: Replace this with your custom Python code
-        result1 = dict["Boston"][1][0]
-    return render_template('index.html', result=result1)
+        result1 = ''
+        event0=events[0]
+        event1=events[1]
+        event2=events[2]
+        event3=events[3]
+        event4=events[4]
+        event5=events[5]
+        event6=events[6]
+        event7=events[7]
+        event8=events[8]
+    return render_template('index.html', result=result1,event0=event0,event1=event1,event2=event2,event3=event3,event4=event4,event5=event5,event6=event6,event7=event7,event8=event8)
 
 if __name__ == '__main__':
     app.run(debug=True)
